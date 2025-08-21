876f483fa6c792c4303b90ab92c240fb9aa68f53
Rafaelagnn <23rafa.goncalves@gmail.com>
2025-08-21 16:22:41 +0100

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,8 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+
+    // rafaela
+
     return 0;
 }
